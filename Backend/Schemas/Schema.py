71d4e6c49ad975540353cc6f67c2b104e4255f8f
Amal Dev <amald1101@gmail.com>
2026-05-10 20:16:04 -0400

    Updated schemas and added session table schema

---
+++ Backend/Schemas/Schema.py
@@ -0,0 +1,41 @@
+import uuid
+from sqlalchemy import Column, DateTime, ForeignKey, String, UUID
+from sqlalchemy.dialects.postgresql import ARRAY
+from Backend.DB import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
+    email = Column(String, unique=True, index=True, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    phone_number = Column(String)
+    password = Column(String, nullable=False)
+    created_date = Column(DateTime)
+    updated_date = Column(DateTime)
+
+class UserPreferences(Base):
+    __tablename__ = "user_preferences"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), index=True)
+    dietary_lifestyle = Column(String)
+    allergies = Column(ARRAY(String))
+    health_conditions = Column(ARRAY(String))
+    nutritional_goals = Column(ARRAY(String))
+    cuisine_preferences = Column(ARRAY(String))
+    cooking_skill_level = Column(String)
+    created_date = Column(DateTime)
+    updated_date = Column(DateTime)
+
+
+class Session(Base):
+    __tablename__ = "sessions"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), index=True)
+    session_token = Column(String, unique=True)
+    ttl = Column(String)
+    created_date = Column(DateTime)
+    updated_date = Column(DateTime)
